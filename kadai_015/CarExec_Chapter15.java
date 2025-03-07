32b84fa2122bb6df55bc28cd7396006b46d47132
tenma-2000 <freedomprogrammer.t@gmail.com>
2025-03-07 18:37:58 +0900

    課題提出

---
+++ kadai_015/CarExec_Chapter15.java
@@ -0,0 +1,17 @@
+package kadai_015;
+
+public class CarExec_Chapter15 {
+
+	public static void main(String[] args) {
+		
+		Car_Charpter15 car  = new Car_Charpter15();
+		
+		
+		car.gearChage(3);
+		
+		car.run();
+		
+
+	}
+
+}
