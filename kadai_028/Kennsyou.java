48940052cea5c096c2d3f4b4e36f1b40c93f81a8
tenma-2000 <freedomprogrammer.t@gmail.com>
2025-03-14 14:36:10 +0900

    課題提出

---
+++ kadai_028/Kennsyou.java
@@ -0,0 +1,21 @@
+package kadai_028;
+
+public class Kennsyou {
+
+	public static void main(String[] args) {
+
+		int r = 1;
+		int s = 2;
+		int p = 3;
+		
+		
+
+		switch (s) {
+		case 1 -> System.out.println("あいこです");
+		case 2 -> System.out.println("自分の勝ちです");
+		default -> System.out.println("自分の負けです");
+		}
+
+	}
+
+}
